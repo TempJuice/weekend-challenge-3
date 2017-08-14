5e98abb55e30618c3ac37c1abe8250f1b42b1512
Ryan Templin <tempjuice@hotmail.com>
2017-08-14 10:19:00 -0500

    More styling.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,82 +1,82 @@
 $(document).ready(function () {
-
-  //Grab current tasks from DB
-  getTasks();
-
-  $('#addButton').on('click', function () {
-    var taskName = $('#taskIn').val();
-    //Generating new task as an object
-    var taskToSend = {
-      task: taskName,
-      done: 'N'
-    };
-    saveTask(taskToSend);
-    $('#taskIn').val('');
-  }); //end addButton event listener
-
-  $('#showTasks').on('click', '.completeButton', function () {
-    var taskId = $(this).parent().parent().data().id;
+  
+    //Grab current tasks from DB
+    getTasks();
+  
+    $('#addButton').on('click', function () {
+      var taskName = $('#taskIn').val();
+      //Generating new task as an object
+      var taskToSend = {
+        task: taskName,
+        done: 'N'
+      };
+      saveTask(taskToSend);
+      $('#taskIn').val('');
+    }); //end addButton event listener
+  
+    $('#showTasks').on('click', '.completeButton', function () {
+      var taskId = $(this).parent().parent().data().id;
+      $.ajax({
+        method: 'PUT',
+        url: '/tasks/' + taskId,
+        success: function (response) {
+          getTasks();
+        }
+      })//end ajax PUT
+    });//end completeButton event listener
+  
+    $('#showTasks').on('click', '.deleteButton', function () {
+      var taskId = $(this).parent().parent().data().id;
+      var warning;
+      var warningBox = confirm("Are you sure you wanna git rid a dis?");
+      if (warningBox == true) {
+          warning = alert("Hope Mama knows...");
+          $.ajax({
+            method: 'DELETE',
+            url: '/tasks/' + taskId,
+            success: function (response) {
+              getTasks();
+            }
+          })//end ajax DELETE
+      } else {
+          warning = alert("Yeaaah, best not make Mama mad...");
+      }
+    });//end deleteButton event listener
+  });// end $(doc).ready
+  
+  function saveTask(newTask) {
     $.ajax({
-      method: 'PUT',
-      url: '/tasks/' + taskId,
-      success: function (response) {
+      url: '/tasks',
+      type: 'POST',
+      data: newTask,
+      success: function (data) {
         getTasks();
       }
-    })//end ajax PUT
-  });//end completeButton event listener
-
-  $('#showTasks').on('click', '.deleteButton', function () {
-    var taskId = $(this).parent().parent().data().id;
-    var warning;
-    var warningBox = confirm("Are you sure you wanna git rid a dis?");
-    if (warningBox == true) {
-        warning = alert("Hope Mama knows...");
-        $.ajax({
-          method: 'DELETE',
-          url: '/tasks/' + taskId,
-          success: function (response) {
-            getTasks();
-          }
-        })//end ajax DELETE
-    } else {
-        warning = alert("Yeaaah, best not make Mama mad...");
-    }
-  });//end deleteButton event listener
-});// end $(doc).ready
-
-function saveTask(newTask) {
-  $.ajax({
-    url: '/tasks',
-    type: 'POST',
-    data: newTask,
-    success: function (data) {
-      getTasks();
-    }
-  }); //end ajax POST
-}//end saveTask()
-
-function getTasks() {
-  $.ajax({
-    url: '/tasks',
-    type: 'GET',
-    success: function (data) {
-      showTasks(data);
-    }
-  }); //end ajax GET
-} // end getTasks()
-
-function showTasks(tasksArray) {
-  $('#showTasks').empty();
-  for (var i = 0; i < tasksArray.length; i++) {
-    var tasks = tasksArray[i];
-    var taskDisplay = $('<tr></tr>');
-    if (tasks.done == "Y") {
-      var taskName = $('<td class="completedTask">' + tasks.task + '<button class="deleteButton">Delete</button></td>');
-    } else {
-      taskName = $('<td class="task">' + tasks.task + '<button class="completeButton">Complete</button>' + '<button class="deleteButton">Delete</button></td>');
-    }
-    taskDisplay.data('id', tasks.id);
-    $('#showTasks').append(taskDisplay);
-    $(taskDisplay).append(taskName);
-  }//end for loop
-};// end showTasks() 
+    }); //end ajax POST
+  }//end saveTask()
+  
+  function getTasks() {
+    $.ajax({
+      url: '/tasks',
+      type: 'GET',
+      success: function (data) {
+        showTasks(data);
+      }
+    }); //end ajax GET
+  } // end getTasks()
+  
+  function showTasks(tasksArray) {
+    $('#showTasks').empty();
+    for (var i = 0; i < tasksArray.length; i++) {
+      var tasks = tasksArray[i];
+      var taskDisplay = $('<tr></tr>');
+      if (tasks.done == "Y") {
+        var taskName = $('<td class="completedTask">' + tasks.task + '<button class="deleteButton">Delete</button></td>');
+      } else {
+        taskName = $('<td class="task">' + tasks.task + '<button class="completeButton">Complete</button>' + '<button class="deleteButton">Delete</button></td>');
+      }
+      taskDisplay.data('id', tasks.id);
+      $('#showTasks').append(taskDisplay);
+      $(taskDisplay).append(taskName);
+    }//end for loop
+  };// end showTasks()
